f82a6a6c067000b199b97ebbdd02cb176ae44104
NehaVanje21 <105428061+NehaVanje21@users.noreply.github.com>
2022-05-18 12:10:06 +0200

    Add files via upload

---
+++ BooleanDataType.java
@@ -0,0 +1,14 @@
+package Neha;
+
+public class BooleanDataType {
+
+	public static void main(String[] args) 
+	{
+		boolean mybool = true;
+		if(mybool == true)
+			System.out.println("i am using a Boolean data type:"+mybool);
+		
+
+	}
+
+}
